90ca73b4f33e8b9753e52503df9d8df678c3eb47
Viet Nguyen <>
2024-11-27 11:44:13 +0700

    MERN STACK - #2 Cấu hình router với react-router-dom-v6 trong reactjs - Lập trình thật dễ

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import type { RootState } from "./redux/store";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./redux/slices/counterSlice";
-import styled from "styled-components";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
+import OrderPage from "./pages/OrderPage/OrderPage";
+import routes from "./routes";
+import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
 
 function App() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
-
-  const Button = styled.button({
-    color: "grey",
-  });
-
   return (
-    <div>
-      Hello World
-      <div>
-        <Button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </Button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        {routes.map((route, index) => {
+          const Layout = route.isShowHeader ? DefaultComponent : Fragment;
+          const Page = route.page;
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </Router>
   );
 }
 
