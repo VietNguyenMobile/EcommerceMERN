fb903f722915f19b879854b3615a5a4d4173c0f6
Viet Nguyen <>
2024-11-29 15:11:10 +0700

    MERN STACK - #7 Tạo card product cho sản phẩm - Lập trình thật dễ

---
+++ src/components/CardComponent/CardComponent.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Card } from "antd";
+import {
+  StyleNameProduct,
+  WrapperReportText,
+  WrapperPriceText,
+  WrapperDiscountText,
+  WrapperCard,
+  WrapperImageStyle,
+} from "./style";
+import { StarFilled } from "@ant-design/icons";
+import { Image } from "antd";
+import logo_official from "../../assets/images/logo_official.png";
+
+const CardComponent = () => {
+  return (
+    <WrapperCard
+      hoverable
+      style={{ width: 240 }}
+      bodyStyle={{ padding: "10px" }}
+      cover={
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <WrapperImageStyle
+        src={logo_official}
+        style={{ position: "absolute", top: 0, left: -1 }}
+        alt="Logo Official"
+      />
+      <StyleNameProduct>iPhone</StyleNameProduct>
+      <WrapperReportText>
+        <span style={{ marginRight: "4px" }}>
+          <span>4.96</span>{" "}
+          <StarFilled style={{ fontSize: "12px", color: "yellow" }} />
+        </span>
+        <span> | Đã bán 1000+</span>
+      </WrapperReportText>
+      <WrapperPriceText>
+        1.000.000 đ<WrapperDiscountText>-10%</WrapperDiscountText>
+      </WrapperPriceText>
+    </WrapperCard>
+  );
+};
+
+export default CardComponent;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import SliderComponent from "../../components/SliderComponent/SliderComponent";
 import slider1 from "../../assets/images/slider1.webp";
 import slider2 from "../../assets/images/slider2.webp";
 import slider3 from "../../assets/images/slider3.webp";
+import CardComponent from "../../components/CardComponent/CardComponent";
 
 const HomePage = () => {
   const arr = ["TV", "Laptop", "Phone"];
@@ -22,6 +23,17 @@ const HomePage = () => {
         style={{ backgroundColor: "#efefef", padding: "0 120px" }}
       >
         <SliderComponent arrImages={[slider1, slider2, slider3]} />
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            alignItems: "center",
+            gap: "20px",
+            height: "1000px",
+          }}
+        >
+          <CardComponent />
+        </div>
       </div>
     </>
   );
