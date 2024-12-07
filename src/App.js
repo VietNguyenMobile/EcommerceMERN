ffea79c7493cb85fa8b840622148a8254d5402ff
Viet Nguyen <>
2024-12-07 17:19:18 +0700

    MERN STACK - #26 Tích hợp react query vào dự án - Lập trình thật dễ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import ProductsPage from "./pages/ProductsPage/ProductsPage";
-import OrderPage from "./pages/OrderPage/OrderPage";
 import routes from "./routes";
 import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
+  // useEffect(() => {
+  //   fetchApi();
+  // }, []);
+
+  const fetchApi = async () => {
+    console.log("fetchApi");
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL}/product/get-details/675402559c0a1f5b4b53afbc`
+    );
+    console.log("res", res);
+    return res.data;
+  };
+
+  const query = useQuery({
+    queryKey: "product",
+    queryFn: fetchApi,
+  });
+  console.log("query", query);
+
   return (
     <Router>
       <Routes>
